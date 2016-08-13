ba078b22e2248af0865c0f6e04803fd027914d44
Lawrence Lai <lawrencelai299@gmail.com>
2016-08-13 23:09:20 +0800

    Port 80 serve for HTML listening
    Port 80 serve for HTML listening

--- src/main/java/org/oriole/Application.java
+++ src/main/java/org/oriole/Application.java
@@ -13,6 +13,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class Application {
 	public static void main(String[] args) {
 	    SpringApplicationBuilder parentBuilder  = new SpringApplicationBuilder(Application.class);
+	    // For HTML Verison Oriole
+	    parentBuilder.child(Application.class).properties("server.port:80").run(args);
+	    // For Webservice Verison Oriole
 	    parentBuilder.child(Application.class).properties("server.port:8080").run(args);
 	}
 
